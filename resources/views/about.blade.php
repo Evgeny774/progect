97b1088bfa4cd697795a0b51390c9d7d9078395c
Evgeny774 <155822187+Evgeny774@users.noreply.github.com>
2024-01-09 18:28:01 +0300

    test
    test

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,6 +5,4 @@
     <img style="display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://a.d-cd.net/DgAAAgHMeuA-1920.jpg" width="1085" height="678">
 
 
-
-
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,9 @@
 @extends('layout')
 
 @section('main_content')
-    <img style="display: block;-webkit-user-select: none;margin: auto;cursor: zoom-in;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://xn--80adxhks.xn----8sbkdskilpjnjd3k.xn--p1ai/upload/iblock/ed9/shk-0112-dobro-pozhalovat-2-5h0-5.jpg" width="1085" height="349">
+
+    <img style="display: block;-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 90%);transition: background-color 300ms;" src="https://i.pinimg.com/736x/25/27/37/252737cde221d2b781572223eaaafc9c.jpg">
+
+
+
 @endsection
